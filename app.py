b29e39ac9a75ef4f0fb70ebad9ea89dc879eb0d3
Ephantus karanja <ephantusslogan@gmail.com>
2018-06-24 20:58:44 +0300

    connect mysql db with the app

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, redirect, flash, url_for, session
-app = Flask(__name__)
+from flask import Flask, render_template, request, logging, redirect, flash, url_for, session
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, validators, TextAreaField, BooleanField
 from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+# config mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'switcher12'
+app.config['MYSQL_DB'] = 'one_minute_pitch'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# init mysql
+mysql = MySQL(app)
+
+
 
 @app.route('/')
 def index():
@@ -13,22 +26,23 @@ def index():
 def pitches():
     return render_template('pitches.html')
 
+
 @app.route('/latest')
 def latest():
     return render_template('latest.html')
 
+
 @app.route('/most_voted')
 def mostVoted():
     return render_template('most_voted.html')
-    
 
 
 @app.route('/favourites')
 def favourites():
     return render_template('favourites.html')
-    
 
-class RegisterForm(Form):
+
+class SignUpForm(Form):
     name = StringField('Name', [validators.length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
@@ -37,6 +51,34 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
+
+
+@app.route('/signup', methods=['Get', 'Post'])
+def signup():
+    form = SignUpForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        flash('You are now registered and can login', 'success')
+        # create cursor
+        cur = mysql.connection.cursor()
+
+        #execute Query
+        cur.execute("INSERT INTO users(name, email, username, password)VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #commit to db
+        mysql.connection.commit()
+
+        #close connection
+        cur.close()
+
+        flash('You are now registered and can login', 'success')
+        return redirect(url_for('index')) 
+
+    return render_template('signup.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
